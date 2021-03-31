d1e8968c3d3771809152e1c0ca73658e1d599adf
JulianBison <80854425+JulianBison@users.noreply.github.com>
2021-03-30 23:23:05 -0300

    Add files via upload

---
+++ ejercicio1clase3.js
@@ -0,0 +1,7 @@
+{
+    let teamName = 'Preparación';
+    let people = [ 'Amalia', 'Karla', 'Silvana', 'Manu', 'Jonh', 'Mike'];
+    let mensaje1 = `Hay ${people.length} personas en el equipo de ${teamName}\n`;
+    let mensaje2 = `Ellos son ${people}`;
+    console.log (mensaje1+mensaje2); 
+}
---
+++ ejercicio2clase3.js
@@ -0,0 +1,3 @@
+let entrada=[1, 2, 3, 4, 5];
+let salida = entrada.map( x=> x % 2==0 ? x*x : x)
+console.log(salida);
